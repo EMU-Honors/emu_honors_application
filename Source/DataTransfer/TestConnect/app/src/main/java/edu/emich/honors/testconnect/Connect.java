1a8e9b3891b191da88725b9bf618708c1c72a5a1
Eddie Gurnee <pegurnee@gmail.com>
2015-04-09 13:10:30 -0400

    added data trasfer test project

---
+++ Source/DataTransfer/TestConnect/app/src/main/java/edu/emich/honors/testconnect/Connect.java
@@ -0,0 +1,101 @@
+package edu.emich.honors.testconnect;
+
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Connect extends ActionBarActivity {
+
+    Button theButton;
+    TextView theOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        this.theButton = (Button) findViewById(R.id.theTestButton);
+        this.theOutput = (TextView) findViewById(R.id.outputTextView);
+
+        this.theButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(),"hi",Toast.LENGTH_LONG).show();
+                theOutput.append(" clicked");
+                Toast.makeText(getBaseContext(), doASomething(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String doASomething() {
+        String result = "";
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://db2.emich.edu/~egurnee/android_test.php");
+        
+        try {
+            List dict = new ArrayList();
+
+            dict.add(new BasicNameValuePair("test", "some data"));
+            post.setEntity(new UrlEncodedFormEntity(dict));
+
+            HttpResponse response = client.execute(post);
+
+            result += EntityUtils.toString(response.getEntity());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return result;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_connect, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
