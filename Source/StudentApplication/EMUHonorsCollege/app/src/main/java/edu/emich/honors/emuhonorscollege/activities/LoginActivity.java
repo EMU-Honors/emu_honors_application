0089a7bc18efde02e51b0db45132af63756e19f5
Travis <ttaylo34@emich.edu>
2015-04-11 19:03:14 -0400

    Added the menu using Navigation Drawer, New options can now be added into the menu

--- Source/StudentApplication/EMUHonorsCollege/app/src/main/java/edu/emich/honors/emuhonorscollege/activities/LoginActivity.java
+++ Source/StudentApplication/EMUHonorsCollege/app/src/main/java/edu/emich/honors/emuhonorscollege/activities/LoginActivity.java
@@ -1,12 +1,19 @@
 package edu.emich.honors.emuhonorscollege.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import edu.emich.honors.emuhonorscollege.R;
 
@@ -15,6 +22,11 @@ public class LoginActivity extends ActionBarActivity {
 
     private Button newUserButton;
     private Button submitButton;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,14 @@ public class LoginActivity extends ActionBarActivity {
 
         newUserButton = (Button) findViewById(R.id.login_new_user);
         submitButton = (Button) findViewById(R.id.login_submit);
+        mDrawerList = (ListView)findViewById(R.id.navList);mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
 
@@ -45,6 +65,11 @@ public class LoginActivity extends ActionBarActivity {
             return true;
         }
 
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -56,5 +81,64 @@ public class LoginActivity extends ActionBarActivity {
     public void submitLoginCredentials(View view)
     {
         startActivity(new Intent(this, ChecklistActivity.class));
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Settings", "Checklist" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch(position) {
+                    case 0:
+                        Intent a = new Intent(LoginActivity.this, SettingsActivity.class);
+                        startActivity(a);
+                        break;
+                    case 1:
+                        Intent b = new Intent(LoginActivity.this, ChecklistActivity.class);
+                        startActivity(b);
+                        break;
+                    default:
+                }
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
--- Source/StudentApplication/EMUHonorsCollege/app/src/main/java/edu/emich/honors/emuhonorscollege/activities/SettingsActivity.java
+++ Source/StudentApplication/EMUHonorsCollege/app/src/main/java/edu/emich/honors/emuhonorscollege/activities/SettingsActivity.java
@@ -1,12 +1,20 @@
 package edu.emich.honors.emuhonorscollege.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import edu.emich.honors.emuhonorscollege.R;
 
@@ -14,14 +22,27 @@ public class SettingsActivity extends ActionBarActivity {
 
     private Button changePasswordButton;
     private Button submitButton;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.settings);
 
         changePasswordButton = (Button) findViewById(R.id.change_password_button);
         submitButton = (Button) findViewById(R.id.settings_submit_button);
+        mDrawerList = (ListView)findViewById(R.id.navList);mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
@@ -43,6 +64,11 @@ public class SettingsActivity extends ActionBarActivity {
             return true;
         }
 
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -54,5 +80,64 @@ public class SettingsActivity extends ActionBarActivity {
     public void onClickPassword(View view)
     {
         startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Checklist", "Back to Login" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(SettingsActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+                switch(position) {
+                    case 0:
+                        Intent a = new Intent(SettingsActivity.this, ChecklistActivity.class);
+                        startActivity(a);
+                        break;
+                    case 1:
+                        Intent b = new Intent(SettingsActivity.this, LoginActivity.class);
+                        startActivity(b);
+                        break;
+                    default:
+                }
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
