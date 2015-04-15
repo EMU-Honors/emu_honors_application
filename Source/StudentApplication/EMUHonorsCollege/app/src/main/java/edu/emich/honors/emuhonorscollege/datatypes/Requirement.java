2f1a7b165295ee16e1f64c78d5609e71a03ca599
Jordan Saunders <jsaunde5@emich.edu>
2015-04-15 17:17:39 -0400

    updated Requirement object to current attributes

--- Source/StudentApplication/EMUHonorsCollege/app/src/main/java/edu/emich/honors/emuhonorscollege/datatypes/Requirement.java
+++ Source/StudentApplication/EMUHonorsCollege/app/src/main/java/edu/emich/honors/emuhonorscollege/datatypes/Requirement.java
@@ -4,31 +4,68 @@ import java.util.ArrayList;
 
 /**
  * Created by stefano on 3/21/15.
+ * 4/15 -> updated to decided attributes of requirements
  */
 public class Requirement{
+
+    private String year;
+    private String type;
+    private String dispNumber;
     private String name;
+    private String component;
+    private int totalNeeded;  //needed for completion
     private String description;
-    private ArrayList<Requirement> subRequirements;
-    private boolean completed;
-    private int numberOfCompleted;
-    private int numberRequiredForCompletion;
-    private int hierarchyLevel;
+    private int completed;  //number completed -> will test if completed == totalNeeded
+
 
     public Requirement() {
+        this.year = "";
+        this.type = "";
+        this.dispNumber = "";
         this.name = "";
+        this.component = "";
+        this.totalNeeded = 0;
         this.description = "";
-        this.subRequirements = new ArrayList<Requirement>();
-        this.completed = false;
-        this.numberOfCompleted = 0;
-        this.numberRequiredForCompletion = 1;
-        this.hierarchyLevel = 1;
+        this.completed = 0;
     }
 
-    public Requirement(String requirementName, String description, int numberRequiredForCompletion) {
+    public Requirement(String year, String type, String dispNumber, String name,
+                       String component, int totalNeeded, String description, int completed) {
         this();
-        this.name = requirementName;
+        this.year = year;
+        this.type = type;
+        this.dispNumber = dispNumber;
+        this.name = name;
+        this.component = component;
+        this.totalNeeded = totalNeeded;
         this.description = description;
-        this.numberRequiredForCompletion = numberRequiredForCompletion;
+        this.completed = completed;
+    }
+
+
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDispNumber() {
+        return dispNumber;
+    }
+
+    public void setDispNumber(String dispNumber) {
+        this.dispNumber = dispNumber;
     }
 
     public String getName() {
@@ -39,6 +76,22 @@ public class Requirement{
         this.name = name;
     }
 
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public int getTotalNeeded() {
+        return totalNeeded;
+    }
+
+    public void setTotalNeeded(int totalNeeded) {
+        this.totalNeeded = totalNeeded;
+    }
+
     public String getDescription() {
         return description;
     }
@@ -47,35 +100,13 @@ public class Requirement{
         this.description = description;
     }
 
-    public int getNumberOfCompleted() {
-        return numberOfCompleted;
-    }
-
-    public void setNumberOfCompleted(int numberOfCompleted) {
-        this.numberOfCompleted = numberOfCompleted;
-        completed = (this.getNumberOfCompleted() >= this.getNumberRequiredForCompletion());
-    }
-
-    public int getNumberRequiredForCompletion() {
-        return numberRequiredForCompletion;
-    }
-
-    public void setNumberRequiredForCompletion(int numberRequiredForCompletion) {
-        this.numberRequiredForCompletion = numberRequiredForCompletion;
-        completed = (this.getNumberOfCompleted() >= this.getNumberRequiredForCompletion());
-    }
-
-    public ArrayList<Requirement> getSubRequirements() {
-        return subRequirements;
-    }
-
-    public boolean isCompleted() {
+    public int getCompleted() {
         return completed;
     }
 
-    public void addSubRequirement(Requirement subRequirementToAdd)
-    {
-        subRequirementToAdd.hierarchyLevel++;
-        subRequirements.add(subRequirementToAdd);
+    public void setCompleted(int completed) {
+        this.completed = completed;
     }
+
+
 }
